b007cd9fc2a9175bfbd6a0fb6c04a64759691bf9
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-03-29 06:54:56 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <section className='px-6 md:px-20 py-24 border-2 border-red-500'>
+      <div className='flex max-xl:flex-col gap-16'>
+        <div className='flex flex-col justify-center'>
+          <p className='small-text'>
+            Choose product wisely
+          </p>
+        </div>
+      </div>
+
+    </section>
+    
+    </>
   )
 }
 
